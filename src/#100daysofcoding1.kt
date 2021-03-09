8fcd38262544ab393d8f35d0b13bf27cfbafb9fa
Neendo_Njuguna <njugunaneendo@gmail.com>
2021-03-09 21:30:11 +0300

    day1

---
+++ src/#100daysofcoding1.kt
@@ -0,0 +1,30 @@
+fun main() {
+    name()
+    age()
+    phonenumber()
+    weight()
+    citizen()
+
+}
+fun name(){
+    var name=("Code")
+    println(name)
+}
+fun age(){
+    var age=25
+    ++age
+    println(age)
+}
+fun phonenumber(){
+    var phonenumber= 70755674
+    println(phonenumber)
+}
+fun weight(){
+    var weight= 50.348
+    println(weight)
+}
+fun citizen(){
+    val kenyanCitizenship:Boolean=true
+    println("true")
+}
+
